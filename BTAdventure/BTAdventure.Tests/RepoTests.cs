54af9623c7d1c753890f7b021e10d1b58720aa9d
Richard Rocque <calvin8824@yahoo.com>
2018-10-23 12:52:07 -0500

    Removed bad auth constructor

--- BTAdventure/BTAdventure.Tests/RepoTests.cs
+++ BTAdventure/BTAdventure.Tests/RepoTests.cs
@@ -10,39 +10,10 @@ namespace BTAdventure.Tests
     [TestFixture]
     class RepoTests
     {
-        // ALRIGHT LADS I HAVE ZERO IDEA AS TO WHAT NEEDS TO BE TESTED HERE.
-        // I do know that it needs to test repo, but not validation because it already pre does that idk.....
         [Test]
-        public void AddPlayer()
+        public void AllEndingsTest()
         {
 
-        }
-
-        [Test]
-        public void RemovePlayer()
-        {
-
-        }
-        [Test]
-        public void OutcomeValidation()
-        {
-
-        }
-        [Test]
-        public void LevelValidation()
-        {
-
-        }
-        [Test]
-        public void GameValidation()
-        {
-
-        }
-        [Test]
-        public void CharacterValidation()
-        {
-
-        }
-
+        } 
     }
 }
