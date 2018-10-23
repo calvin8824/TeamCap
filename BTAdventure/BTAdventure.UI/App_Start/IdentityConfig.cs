d01ded0b0fb60addafbf4c68e7a277f15b8fef3a
Mason Berge <m222109@hotmail.com>
2018-10-23 14:17:13 -0500

    estdsgrsrsyset

--- BTAdventure/BTAdventure.UI/App_Start/IdentityConfig.cs
+++ BTAdventure/BTAdventure.UI/App_Start/IdentityConfig.cs
@@ -11,9 +11,24 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using BTAdventure.UI.Models;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
 
 namespace BTAdventure.UI
 {
+
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
+        }
+    }
+
     public class EmailService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
