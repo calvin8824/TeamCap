cdef86d5ba5f1ece5334922fdb8d684950a4194f
Richard Rocque <calvin8824@yahoo.com>
2018-10-24 14:40:35 -0500

    Converted envent id's to nullable.

--- BTAdventure/BTAdventure/Interfaces/IEventChoiceRepository.cs
+++ BTAdventure/BTAdventure/Interfaces/IEventChoiceRepository.cs
@@ -10,7 +10,7 @@ namespace BTAdventure.Interfaces
     public interface IEventChoiceRepository
     {
         IEnumerable<EventChoice> All();
-        EventChoice FindById(int id);
+        EventChoice FindById(int? id);
         EventChoice Save(EventChoice level);
         bool Delete(int id);
         EventChoice FindBySceneId(int sceneRoute);
