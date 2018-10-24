5f6e38422795edd209a5579ad655104613053f65
Danny Lithyouvong <dannylith@live.com>
2018-10-24 14:25:46 -0500

    added admin stuff

--- BTAdventure/BTAdventure.UI/Controllers/AdminController.cs
+++ BTAdventure/BTAdventure.UI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using BTAdventure.Services;
+﻿using BTAdventure.Models;
+using BTAdventure.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +21,71 @@ namespace BTAdventure.UI.Controllers
             return View();
         }
 
-        // GET: Admin
+        // this will probably be removed. will look into it later
         public ActionResult ListGames()
         {
             var allScene = adminService.GetAllScenes();
             return View(allScene);
         }
+
+        public ActionResult NewOrEditGame(int id = 0)
+        {
+            Game game = new Game();
+            if (id>0)
+            {
+                game = adminService.GetAllGames().Where(g => g.GameId == id).FirstOrDefault();
+            }
+            
+            return View(game);
+        }
+
+
+        public ActionResult EditGame()
+        {
+            var allGames = adminService.GetAllGames();
+
+            //this is mock data
+            //var something = new Game
+            //{
+            //    GameId = 1,
+            //    GameTitle = "The Title",
+            //    IntroText = "The Intro"
+            //};
+            //allGames.Add(something);
+            //allGames.Add(something);
+            //allGames.Add(something);
+            //allGames.Add(something);
+            //allGames.Add(something);
+            return View(allGames);
+        }
+
+        public ActionResult SceneMain(int id = 0)
+        {
+            IEnumerable<Scene> allScenesFromGameId = new List<Scene>();
+            if (id>0)
+            {
+                allScenesFromGameId = adminService.GetAllScenes().Where(s => s.GameId == id);
+            }
+            
+            return View(allScenesFromGameId);
+        }
+
+        [HttpPost]
+        public ActionResult SceneMain(Game game)
+        {
+            IEnumerable<Scene> allScenesFromGameId = new List<Scene>();
+            if (game.GameId > 0)
+            {
+                allScenesFromGameId = adminService.GetAllScenes().Where(s => s.GameId == game.GameId);
+            }
+
+            return View(allScenesFromGameId);
+        }
+
+        public ActionResult CreateOrEditScene()
+        {
+            return View();
+        }
+
     }
 }
