339bd9b50d56c8632830b6d5b453861037ff85fe
Stephen Erickson <stephen_erickson04@yahoo.com>
2018-10-26 13:58:49 -0500

    kk

--- SqlFiles/ExampleQuery.sql
+++ SqlFiles/ExampleQuery.sql
@@ -13,4 +13,8 @@ Select * From Ending
 Select * From Outcome
 Select * From EventChoice
 Select * From AspNetUsers
+Select * FROM AspNetUserRoles
 
+UPDATE PlayerCharacter
+SET PlayerId = 'fb06fcce-40d5-4854-ba35-1378b20960af'
+WHERE CharacterId = 1
